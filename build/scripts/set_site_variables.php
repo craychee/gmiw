0a506c26544d8937365e6b9bc0c2487ffa6cb3c4
Michelle Krejci <michelle.krejci@pantheon.io>
2015-10-09 17:27:46 -0700

    Adding caching.

--- build/scripts/set_site_variables.php
+++ build/scripts/set_site_variables.php
@@ -2,7 +2,10 @@
 $enable = array(
   'site_frontpage' => 'node',
   'site_name' => 'Gosh, Michelle is Witty',
-  'site_slogan' => "Witty things I think of that really impress me."
+  'site_slogan' => "Witty things I think of that really impress me.",
+  'cache_lifetime' => "900",
+  'block_cache' => 1,
+  'cache' => 1,
 );
 foreach ($enable as $var => $setting) {
   if (!is_numeric($var)) {
